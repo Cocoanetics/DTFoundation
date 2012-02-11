458894cecc718ae934bfd2ffc9a24f00f1bd7ea7
Oliver Drobnik <oliver@drobnik.com>
2012-02-11 16:26:30 +0100

    Added a second rename queue so that the renaming blocks, but does not have to wait for a previously scheduled removal to finish.

--- Core/Source/DTAsyncFileDeleter.m
+++ Core/Source/DTAsyncFileDeleter.m
@@ -13,6 +13,9 @@ static dispatch_queue_t _delQueue;
 static dispatch_group_t _delGroup;
 static dispatch_once_t onceToken;
 
+static dispatch_queue_t _renameQueue;
+static dispatch_group_t _renameGroup;
+
 static DTAsyncFileDeleter *_sharedInstance;
 
 @implementation DTAsyncFileDeleter
@@ -33,8 +36,10 @@ static DTAsyncFileDeleter *_sharedInstance;
 	if (self)
 	{
 		dispatch_once(&onceToken, ^{
-			_delQueue = dispatch_queue_create("DTAsyncFileDeleterQueue", 0);
+			_delQueue = dispatch_queue_create("DTAsyncFileDeleterRemoveQueue", 0);
 			_delGroup = dispatch_group_create();
+			_renameQueue = dispatch_queue_create("DTAsyncFileDeleterRenameQueue", 0);
+			_renameGroup = dispatch_group_create();
 		});
 		
 		[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(applicationDidEnterBackground:) name:UIApplicationDidEnterBackgroundNotification object:nil];
@@ -50,30 +55,35 @@ static DTAsyncFileDeleter *_sharedInstance;
 
 - (void)waitUntilFinished
 {
+	dispatch_group_wait(_renameGroup, DISPATCH_TIME_FOREVER);
 	dispatch_group_wait(_delGroup, DISPATCH_TIME_FOREVER);
 }
 
 - (void)removeItemAtPath:(NSString *)path
 {
-	dispatch_group_async(_delGroup, _delQueue, ^{
-		// move it to a tmp name to that it appears gone
-		CFUUIDRef newUniqueId = CFUUIDCreate(kCFAllocatorDefault);
-		CFStringRef newUniqueIdString = CFUUIDCreateString(kCFAllocatorDefault, newUniqueId);
-		NSString *tmpPath = [NSTemporaryDirectory() stringByAppendingPathComponent:(__bridge NSString *)newUniqueIdString];
-		CFRelease(newUniqueId);
-		CFRelease(newUniqueIdString);
-		
-		// make a file manager just for this thread/queue
+	// make a unique temporary name in tmp folder
+	CFUUIDRef newUniqueId = CFUUIDCreate(kCFAllocatorDefault);
+	CFStringRef newUniqueIdString = CFUUIDCreateString(kCFAllocatorDefault, newUniqueId);
+	NSString *tmpPath = [NSTemporaryDirectory() stringByAppendingPathComponent:(__bridge NSString *)newUniqueIdString];
+	CFRelease(newUniqueId);
+	CFRelease(newUniqueIdString);
+	
+	// rename the file, AFTER previous renames
+	dispatch_group_async(_renameGroup, _renameQueue, ^{
 		NSFileManager *fileManager = [[NSFileManager alloc] init];
 		
-		if (![fileManager moveItemAtPath:path toPath:tmpPath error:NULL])
+		if ([fileManager moveItemAtPath:path toPath:tmpPath error:NULL])
 		{
-			// looks like the file is no longer there
-			return;
+			// schedule the removal and immediately return	
+			dispatch_group_async(_delGroup, _delQueue, ^{
+				// file manager is not used any more in the rename queue, so we reuse it
+				[fileManager removeItemAtPath:tmpPath error:NULL];
+			});
 		}
-		
-		[fileManager removeItemAtPath:tmpPath error:NULL];
 	});
+	
+	// block until all renames are done
+	dispatch_group_wait(_renameGroup, DISPATCH_TIME_FOREVER);
 }
 
 - (void)removeItemAtURL:(NSURL *)URL
