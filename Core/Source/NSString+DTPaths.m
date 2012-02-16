9d5a7cdd364984bf4812e68ac0509531ac127608
Oliver Drobnik <oliver@drobnik.com>
2012-02-16 11:41:24 +0100

    Added temporary path handling

--- Core/Source/NSString+DTPaths.m
+++ Core/Source/NSString+DTPaths.m
@@ -13,6 +13,8 @@ MAKE_CATEGORIES_LOADABLE(NSString_DTPaths);
 
 @implementation NSString (DTPaths)
 
+#pragma mark Standard Paths
+
 + (NSString *)cachesPath
 {
 	static dispatch_once_t onceToken;
@@ -35,6 +37,31 @@ MAKE_CATEGORIES_LOADABLE(NSString_DTPaths);
 	});
 
 	return cachedPath;
+}
+
+#pragma mark Temporary Paths
+
++ (NSString *)temporaryPath
+{
+	static dispatch_once_t onceToken;
+	static NSString *cachedPath;
+	
+	dispatch_once(&onceToken, ^{
+		cachedPath = NSTemporaryDirectory();	
+	});
+	
+	return cachedPath;
+}
+
++ (NSString *)pathForTemporaryFile
+{
+	CFUUIDRef newUniqueId = CFUUIDCreate(kCFAllocatorDefault);
+	CFStringRef newUniqueIdString = CFUUIDCreateString(kCFAllocatorDefault, newUniqueId);
+	NSString *tmpPath = [NSTemporaryDirectory() stringByAppendingPathComponent:(__bridge NSString *)newUniqueIdString];
+	CFRelease(newUniqueId);
+	CFRelease(newUniqueIdString);
+	
+	return tmpPath;
 }
 
 @end
