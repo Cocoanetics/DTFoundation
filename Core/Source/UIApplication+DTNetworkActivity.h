24785804a22ba52fc21cb2297fec6c1df6a6c5ac
Oliver Drobnik <oliver@drobnik.com>
2012-05-24 15:39:28 +0200

    Various Updates and Fixes

---
+++ Core/Source/UIApplication+DTNetworkActivity.h
@@ -0,0 +1,13 @@
+//
+//  UIApplication+DTNetworkActivity.h
+//  DTFoundation
+//
+//  Created by Oliver Drobnik on 5/21/12.
+//  Copyright (c) 2012 Cocoanetics. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIApplication (DTNetworkActivity)
+
+@end
---
+++ Core/Source/UIApplication+DTNetworkActivity.m
@@ -0,0 +1,13 @@
+//
+//  UIApplication+DTNetworkActivity.m
+//  DTFoundation
+//
+//  Created by Oliver Drobnik on 5/21/12.
+//  Copyright (c) 2012 Cocoanetics. All rights reserved.
+//
+
+#import "UIApplication+DTNetworkActivity.h"
+
+@implementation UIApplication (DTNetworkActivity)
+
+@end
