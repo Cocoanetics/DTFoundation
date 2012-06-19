76a1614a81efaf7728190f2c99ba1dc9b86e18a7
Oliver Drobnik <oliver@drobnik.com>
2012-06-19 13:52:23 +0200

    Added UIWebView convenience methods

---
+++ Core/Source/UIWebView+DTFoundation.h
@@ -0,0 +1,19 @@
+//
+//  UIWebView+DTFoundation.h
+//  DTFoundation
+//
+//  Created by Oliver Drobnik on 25.05.12.
+//  Copyright (c) 2012 Cocoanetics. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIWebView (DTFoundation)
+
+/**
+ Getting the current document's title
+ @returns A string with the document title
+ */
+- (NSString *)documentTitle;
+
+@end
