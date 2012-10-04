5f332c661762f8e2045c4119ebac6e0ba4e137ca
Oliver Drobnik <oliver@drobnik.com>
2012-10-04 11:44:33 +0200

    Added global system header to aggregate header to allow inclusion via PCH

--- Core/DTFoundation.h
+++ Core/DTFoundation.h
@@ -1,22 +1,56 @@
+//
+// Main aggregate header for 'DTFoundation'
+//
+
+// Global System Headers
+// this prevents problems if you include DTFoundation.h in your PCH file but are missing these other system frameworks
+#if TARGET_OS_IPHONE
+	#import <UIKit/UIKit.h>
+	#import <MobileCoreServices/MobileCoreServices.h>
+#else
+	#import <AppKit/AppKit.h>
+	#import <Cocoa/Cocoa.h>
+#endif
+
 // Classes
 #import "DTASN1Parser.h"
 #import "DTAsyncFileDeleter.h"
 #import "DTDownload.h"
+#import "DTExtendedFileAttributes.h"
 #import "DTHTMLParser.h"
+#import "DTPDFDocument.h"
 #import "DTVersion.h"
 #import "DTZipArchive.h"
 
+#if TARGET_OS_IPHONE
+	#import "DTPieProgressIndicator.h"
+	#import "DTActionSheet.h"
+#endif
+
 // Categories
+#import "NSArray+DTError.h"
+#import "NSData+Base64.h"
+#import "NSData+DTCrypto.h"
 #import "NSDictionary+DTError.h"
+#import "NSMutableArray+DTMoving.h"
+#import "NSObject+DTRuntime.h"
 #import "NSString+DTFormatNumbers.h"
 #import "NSString+DTPaths.h"
 #import "NSString+DTURLEncoding.h"
+#import "NSString+DTUTI.h"
 #import "NSURL+DTAppLinks.h"
-#import "NSURL+DTPrefLinks.h"
+#import "NSURL+DTUnshorten.h"
 
 #if TARGET_OS_IPHONE
-#import "UIImage+DTFoundation.h"
-#import "UIView+DTFoundation.h"
+	#import "UIApplication+DTNetworkActivity.h"
+	#import "UIImage+DTFoundation.h"
+	#import "UIView+DTFoundation.h"
+	#import "UIWebView+DTFoundation.h"
+	#import "UIView+DTActionHandlers.h"
+#else
+	#import "NSImage+DTUtilities.h"
+	#import "NSDocument+DTFoundation.h"
+	#import "NSWindowController+DTPanelControllerPresenting.h"
 #endif
 
 // Utility Functions
