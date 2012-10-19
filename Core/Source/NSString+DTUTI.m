0ab3af090c6b1a183013e8d0d881cf2079c9621b
Oliver Drobnik <oliver@drobnik.com>
2012-10-19 18:00:07 +0200

    Added new UTI convenience methods

--- Core/Source/NSString+DTUTI.m
+++ Core/Source/NSString+DTUTI.m
@@ -23,4 +23,54 @@
 	return result;
 }
 
++ (NSString *)fileTypeDescriptionForFileExtension:(NSString *)extension
+{
+	CFStringRef typeForExt = UTTypeCreatePreferredIdentifierForTag(kUTTagClassFilenameExtension,(__bridge CFStringRef)extension , NULL);
+	
+	return (__bridge_transfer NSString *)UTTypeCopyDescription(typeForExt);
+}
+
++ (NSString *)universalTypeIdentifierForFileExtension:(NSString *)extension
+{
+	return (__bridge_transfer NSString *)UTTypeCreatePreferredIdentifierForTag(kUTTagClassFilenameExtension,(__bridge CFStringRef)extension , NULL);
+}
+
+- (BOOL)conformsToUniversalTypeIdentifier:(NSString *)conformingUTI
+{
+	return UTTypeConformsTo((__bridge CFStringRef)(self), (__bridge CFStringRef)conformingUTI);
+}
+
+- (BOOL)isMovieFileName
+{
+	NSString *extension = [self pathExtension];
+	NSString *uti = [NSString universalTypeIdentifierForFileExtension:extension];
+
+	return [uti conformsToUniversalTypeIdentifier:@"public.movie"];
+}
+
+- (BOOL)isAudioFileName
+{
+	NSString *extension = [self pathExtension];
+	NSString *uti = [NSString universalTypeIdentifierForFileExtension:extension];
+	
+	return [uti conformsToUniversalTypeIdentifier:@"public.audio"];
+}
+
+- (BOOL)isImageFileName
+{
+	NSString *extension = [self pathExtension];
+	NSString *uti = [NSString universalTypeIdentifierForFileExtension:extension];
+	
+	return [uti conformsToUniversalTypeIdentifier:@"public.image"];
+}
+
+- (BOOL)isHTMLFileName
+{
+	NSString *extension = [self pathExtension];
+	NSString *uti = [NSString universalTypeIdentifierForFileExtension:extension];
+	
+	return [uti conformsToUniversalTypeIdentifier:@"public.html"];
+}
+
+
 @end
