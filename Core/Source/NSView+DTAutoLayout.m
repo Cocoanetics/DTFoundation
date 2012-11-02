1e606d3cbdfc912a5aee661069899611ce7e7c65
Oliver Drobnik <oliver@drobnik.com>
2012-11-02 16:23:48 +0100

    Added NSView autolayout helper

---
+++ Core/Source/NSView+DTAutoLayout.m
@@ -0,0 +1,19 @@
+//
+//  NSView+DTAutoLayout.m
+//  DTFoundation
+//
+//  Created by Oliver Drobnik on 26.10.12.
+//  Copyright (c) 2012 Cocoanetics. All rights reserved.
+//
+
+#import "NSView+DTAutoLayout.h"
+
+@implementation NSView (DTAutoLayout)
+
+- (void)addLayoutConstraintWithWidthGreaterOrEqualThan:(CGFloat)width
+{
+	NSLayoutConstraint *constraint = [NSLayoutConstraint constraintWithItem:self attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationGreaterThanOrEqual toItem:nil attribute:NSLayoutAttributeWidth multiplier:1 constant:width];
+	[self addConstraint:constraint];
+}
+
+@end
