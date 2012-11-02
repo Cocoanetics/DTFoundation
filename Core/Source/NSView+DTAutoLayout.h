f89f994d72fe05b87b6c0b6c6bd22f0f10e1999a
Oliver Drobnik <oliver@drobnik.com>
2012-11-02 16:23:48 +0100

    Added NSView autolayout helper

---
+++ Core/Source/NSView+DTAutoLayout.h
@@ -0,0 +1,13 @@
+//
+//  NSView+DTAutoLayout.h
+//  DTFoundation
+//
+//  Created by Oliver Drobnik on 26.10.12.
+//  Copyright (c) 2012 Cocoanetics. All rights reserved.
+//
+
+@interface NSView (DTAutoLayout)
+
+- (void)addLayoutConstraintWithWidthGreaterOrEqualThan:(CGFloat)width;
+
+@end
