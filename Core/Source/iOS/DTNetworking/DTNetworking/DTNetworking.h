b401132c8afb318b4dd99f52b5de173e0b8bab50
Oliver Drobnik <oliver@cocoanetics.com>
2013-01-17 13:03:53 +0100

    Removed DTDownload and DTBonjour
    They are now in their own GitHub repos.

---
+++ Core/Source/iOS/DTNetworking/DTNetworking/DTNetworking.h
@@ -0,0 +1,13 @@
+//
+//  DTNetworking.h
+//  DTNetworking
+//
+//  Created by Oliver Drobnik on 17.01.13.
+//  Copyright (c) 2013 Drobnik KG. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DTNetworking : NSObject
+
+@end
