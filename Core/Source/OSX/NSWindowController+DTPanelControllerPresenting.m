558a8e7c8ef15485bf6bf5cfa273414eff553ee7
Oliver Drobnik <oliver@cocoanetics.com>
2013-01-31 16:53:57 +0100

    Fixed a potential crash Apparently you shouldn't release the NSViewController too early.

--- Core/Source/OSX/NSWindowController+DTPanelControllerPresenting.m
+++ Core/Source/OSX/NSWindowController+DTPanelControllerPresenting.m
@@ -41,12 +41,18 @@ static char DTPresentedViewControllerKey;
 
     // force it onto next run loop to prevent sendAction exception
     dispatch_async(dispatch_get_main_queue(), ^{
-        [NSApp endSheet:windowController.window];
         [windowController.window close];
+        [NSApp endSheet:windowController.window];
+//        [windowController.window orderOut:nil];
     });
-
-	// free the panel view controller
-	objc_setAssociatedObject(self, &DTPresentedViewControllerKey, nil, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    
+    //need to hold onto this during animation, or else we get a crash
+    double delayInSeconds = 0.40;
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+        // free the panel view controller
+        objc_setAssociatedObject(self, &DTPresentedViewControllerKey, nil, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    });
 }
 
 - (NSWindowController *)modalPanelController
