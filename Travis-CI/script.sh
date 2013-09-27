e5ce6d574e41b7d5add6fdbca071497c09d6a815
Oliver Drobnik <oliver@cocoanetics.com>
2013-09-27 17:47:48 +0200

    Try ONLY_ACTIVE_ARCH=NO

--- Travis-CI/script.sh
+++ Travis-CI/script.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
 
-xctool -project DTFoundation.xcodeproj -scheme "Static Library" build test -sdk iphonesimulator -arch i386
+xctool -project DTFoundation.xcodeproj -scheme "Static Library" build test -sdk iphonesimulator -arch i386 ONLY_ACTIVE_ARCH=NO
