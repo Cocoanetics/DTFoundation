b692125d694750a7131096415546b970dfc94b8c
Oliver Drobnik <oliver@cocoanetics.com>
2013-09-27 17:51:06 +0200

    Try CURRENT_ARCH

--- Travis-CI/script.sh
+++ Travis-CI/script.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 set -e
 
-xctool -project DTFoundation.xcodeproj -scheme "Static Library" build test -sdk iphonesimulator -arch i386 ONLY_ACTIVE_ARCH=NO
+#xctool -project DTFoundation.xcodeproj -scheme "Static Library" build test -sdk iphonesimulator -arch i386 ONLY_ACTIVE_ARCH=NO
+xctool -project DTFoundation.xcodeproj -scheme "Static Library" build test -sdk iphonesimulator -arch i386 CURRENT_ARCH=i386
