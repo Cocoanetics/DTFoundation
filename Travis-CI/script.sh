cefe52b0d72f5611ad74efd2ac3112c4d70ebc3e
Oliver Drobnik <oliver@cocoanetics.com>
2013-09-27 17:54:49 +0200

    Reverted to working setting

--- Travis-CI/script.sh
+++ Travis-CI/script.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 set -e
 
-#xctool -project DTFoundation.xcodeproj -scheme "Static Library" build test -sdk iphonesimulator -arch i386 ONLY_ACTIVE_ARCH=NO
-xctool -project DTFoundation.xcodeproj -scheme "Static Library" build test -sdk iphonesimulator -arch i386 CURRENT_ARCH=i386
+xctool -project DTFoundation.xcodeproj -scheme "Static Library" build test -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO
