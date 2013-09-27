a907b764483a28c500beffc9b9ad129f405edfcb
Oliver Drobnik <oliver@cocoanetics.com>
2013-09-27 18:08:04 +0200

    Added unit tests for Travis and build for Mac

--- Travis-CI/script.sh
+++ Travis-CI/script.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 set -e
 
-xctool -project DTFoundation.xcodeproj -scheme "Static Library" build test -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO
+xctool -project DTFoundation.xcodeproj -scheme "Static Library" build test -sdk iphonesimulator -arch i386 ONLY_ACTIVE_ARCH=NO
+xctool -project DTFoundation.xcodeproj -scheme "Static Library (Mac)" build -arch x86_64
