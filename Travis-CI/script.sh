bdf6585166ce6a37cadfc0b8b3351ce676b4e69e
Oliver Drobnik <oliver@cocoanetics.com>
2013-09-30 18:32:23 +0200

    removed travis folder

--- Travis-CI/script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -e
-
-xctool -project DTFoundation.xcodeproj -scheme "Static Library" build test -sdk iphonesimulator -arch i386 ONLY_ACTIVE_ARCH=NO
-xctool -project DTFoundation.xcodeproj -scheme "Static Library (Mac)" build -arch x86_64
