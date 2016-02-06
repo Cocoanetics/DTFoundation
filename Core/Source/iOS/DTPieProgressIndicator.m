0f7799c0071e74afccce891e668479e6a99d3e66
Oliver Drobnik <oliver@cocoanetics.com>
2016-02-06 21:31:09 +0100

    Adjusted DTPieProgressIndicator to use tintColor by default -> makes it useful as subview of UIVisualEffectsView with vibrancy.

--- Core/Source/iOS/DTPieProgressIndicator.m
+++ Core/Source/iOS/DTPieProgressIndicator.m
@@ -41,8 +41,6 @@
 	self.backgroundColor = [UIColor clearColor];
 }
 
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect
 {
 	// Drawing code
@@ -54,7 +52,7 @@
 	}
 	else 
 	{
-		[[UIColor whiteColor] set];
+		[self.tintColor set];
 	}
 	
 	CGContextBeginTransparencyLayer(ctx, NULL);
@@ -82,6 +80,11 @@
 	CGContextEndTransparencyLayer(ctx);
 }
 
+- (void)tintColorDidChange
+{
+	[super tintColorDidChange];
+	[self setNeedsDisplay];
+}
 
 #pragma mark Properties
 
@@ -104,8 +107,5 @@
 		[self setNeedsDisplay];
 	}
 }
-
-@synthesize progressPercent = _progressPercent;
-@synthesize color = _color;
 
 @end
