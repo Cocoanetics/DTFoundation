9171173e4f6c9fe0477c2fa072c2e917cb6ffd05
Stefan Gugarel <stefan@cocoanetics.com>
2016-03-24 13:29:12 +0100

    Fixed compiler warning for iOS9

--- Core/Source/iOS/DTProgressHUD/DTProgressHUDWindow.m
+++ Core/Source/iOS/DTProgressHUD/DTProgressHUDWindow.m
@@ -60,7 +60,7 @@ static CGAffineTransform _transformForInterfaceOrientation(UIInterfaceOrientatio
 		[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(statusBarDidChangeFrame:) name:UIApplicationDidChangeStatusBarOrientationNotification object:nil];
 		
 		// set initial transform
-        UIInterfaceOrientation orientation = self.rootViewController.interfaceOrientation;
+		UIInterfaceOrientation orientation = [[UIApplication sharedApplication] statusBarOrientation];
 		[self setTransform:_transformForInterfaceOrientation(orientation)];
 	}
 	return self;
@@ -75,7 +75,7 @@ static CGAffineTransform _transformForInterfaceOrientation(UIInterfaceOrientatio
 
 - (void)statusBarDidChangeFrame:(NSNotification *)notification
 {
-	UIInterfaceOrientation orientation = self.rootViewController.interfaceOrientation;
+	UIInterfaceOrientation orientation = [[UIApplication sharedApplication] statusBarOrientation];
 	[self setTransform:_transformForInterfaceOrientation(orientation)];
 }
 
