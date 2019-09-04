44d5f2c8407faf9b90731d6a793328fe92681f33
solos <lxl1217@gmail.com>
2019-09-04 21:05:04 +0800

    fix url link contains Chinese chars
    fix url link contains Chinese chars

--- Test/Source/NSStringDTURLEncodingTest.m
+++ Test/Source/NSStringDTURLEncodingTest.m
@@ -22,6 +22,45 @@
 	NSString *email = @"oliver+test@cocoanetics.com";
 	NSString *output = [email stringByURLEncoding];
 	NSString *result = @"oliver%2Btest%40cocoanetics.com";
+	XCTAssertTrue([output isEqualToString:result]);
+}
+
+- (void)testUrlContainsOnlyEnglish
+{
+    NSString *url = @"https://www.example.com/justenglish#hello";
+	NSString *output = [url stringByURLEncoding];
+	NSString *result = @"https%3A%2F%2Fwww.example.com%2Fjustenglish%23hello";
+    NSURL *uri = [NSURL URLWithString:output];
+	XCTAssertTrue([output isEqualToString:result]);
+	XCTAssertFalse([uri isEqual:[NSNull null]]);
+}
+
+- (void)testUrlContainsQueryParam
+{
+    NSString *url = @"https://www.example.com/notice?title=hello&name=world";
+	NSString *output = [url stringByURLEncoding];
+	NSString *result = @"https%3A%2F%2Fwww.example.com%2Fnotice%3Ftitle%3Dhello%26name%3Dworld";
+    NSURL *uri = [NSURL URLWithString:output];
+	XCTAssertTrue([output isEqualToString:result]);
+	XCTAssertFalse([uri isEqual:[NSNull null]]);
+}
+
+
+- (void)testUrlContainsChinese
+{
+    NSString *url = @"https://www.example.com/你好";
+	NSString *output = [url stringByURLEncoding];
+	NSString *result = @"https%3A%2F%2Fwww.example.com%2F%E4%BD%A0%E5%A5%BD";
+    NSURL *uri = [NSURL URLWithString:output];
+	XCTAssertTrue([output isEqualToString:result]);
+	XCTAssertFalse([uri isEqual:[NSNull null]]);
+}
+
+- (void)testUrlContainsChineseAndEnglish
+{
+    NSString *url = @"https://www.example.com/just做it";
+	NSString *output = [url stringByURLEncoding];
+	NSString *result = @"https%3A%2F%2Fwww.example.com%2Fjust%E5%81%9Ait";
 	
 	XCTAssertTrue([output isEqualToString:result]);
 }
