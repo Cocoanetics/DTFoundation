901c02ad95c96ac29ac677b50b8f961e9a6e91b4
Oliver Drobnik <oliver@cocoanetics.com>
2020-09-29 16:11:54 +0200

    Updated Swift Package for 5.3

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -21,6 +21,7 @@ let package = Package(
             name: "DTFoundation",
             dependencies: [],
             path: "Core",
+			exclude: ["DTFoundation-Info.plist", "DTFoundation-Prefix.pch"],
             cSettings: [
                 .headerSearchPath("include/DTFoundation"),
                 .headerSearchPath("Source/Externals/minizip"),
@@ -30,6 +31,26 @@ let package = Package(
             name: "DTFoundationTests",
             dependencies: ["DTFoundation"],
             path: "Test",
+			exclude: ["UnitTests-Info.plist", "UnitTests-Prefix.pch"],
+			resources: [.copy("SelfSigned.der"),
+				.copy("Resources/zipContent/Franz.txt"),
+				.copy("Resources/zipContent/Oliver.txt"),
+				.copy("Resources/zipContent/Stefan.txt"),
+				.copy("Resources/gzip_sample.txt.original"),
+				.copy("Resources/gzip_sample.txt-z"),
+				.copy("Resources/zipContent/Rene"),
+				.copy("Resources/gzip_sample.txt.gz"),
+				.copy("Resources/gzip_sample_invalid.gz"),
+				.copy("Resources/gzip_sample.txt.foo"),
+				.copy("Resources/processing_instruction.html"),
+				.copy("Resources/gzip_sample.txt-gz"),
+				.copy("Resources/html_doctype.html"),
+				.copy("Resources/DictionarySample.plist"),
+				.copy("Resources/ArraySample.plist"),
+				.copy("Resources/screenshot.png"),
+				.copy("Resources/sample.zip"),
+				.copy("Resources/SelfSigned.der")
+			],
             cSettings: [
                 .headerSearchPath("include"),
             ]
--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import DTFoundationTests
-
-var tests = [XCTestCaseEntry]()
-tests += DTFoundationTests.allTests()
-XCTMain(tests)
