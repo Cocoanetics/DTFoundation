b45158c967a9fef4def859482e470ec73b46a2d4
Oliver Drobnik <oliver@cocoanetics.com>
2020-10-07 09:48:33 +0200

    Fix SPM unit tests for Mac

--- Test/Source/DTActionSheetTest.m
+++ Test/Source/DTActionSheetTest.m
@@ -6,12 +6,12 @@
 //  Copyright (c) 2014 Cocoanetics. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
-#import <XCTest/XCTest.h>
+@import DTFoundation
 
-@import DTFoundation;
+#if TARGET_OS_IPHONE && !TARGET_OS_TV && __IPHONE_OS_VERSION_MIN_REQUIRED < 80000
 
-#if !TARGET_OS_TV && __IPHONE_OS_VERSION_MIN_REQUIRED < 80000
+@import UIKit
+@import XCTest
 
 @interface DTActionSheet(Private) <UIActionSheetDelegate>
 @end
--- Test/Source/DTAlertViewTest.m
+++ Test/Source/DTAlertViewTest.m
@@ -6,12 +6,12 @@
 //  Copyright (c) 2014 Cocoanetics. All rights reserved.
 //
 
-@import DTFoundation;
+@import DTFoundation
 
-#if !TARGET_OS_TV && __IPHONE_OS_VERSION_MIN_REQUIRED < 80000
+#if TARGET_OS_IPHONE && !TARGET_OS_TV && __IPHONE_OS_VERSION_MIN_REQUIRED < 80000
 
-@import UIKit;
-@import XCTest;
+@import UIKit
+@import XCTest
 
 @interface DTAlertView(Private) <UIAlertViewDelegate>
 @end
