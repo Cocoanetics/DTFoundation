888456bb8c12ff16888692c021b293daf9f07b0d
Vincent Leclerc <vleclerc@summit-tech.ca>
2021-05-03 15:37:16 -0400

    added swift flags

--- Package.swift
+++ Package.swift
@@ -25,6 +25,8 @@ let package = Package(
             cSettings: [
                 .headerSearchPath("include/DTFoundation"),
                 .headerSearchPath("Source/Externals/minizip"),
+                .unsafeFlags(["-fembed-bitcode"]),
+                .define("BITCODE_GENERATION_MODE", to: "bitcode")
             ]
         ),
         .testTarget(
